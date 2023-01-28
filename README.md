864f80d898a86549358e40026902b6da694af144
plukas <lukas.portier@ynov.com>
2023-01-28 22:56:02 +0100

    Update struct

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - In CLI
 - Adapt to website
 - Adapt in Discord Bot
+
+![Go](https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white)
--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package calculator
 
 type Items struct {
-	Name               string
-	BluePrint          bool
-	BluePrintCostTable int
-	BluePrintCostBench int
-	Workbench          bool
-	WorkbenchLevel     string
-	Stackable          int
-	Craftable          bool
-	AmountCraft        int
-	RessourceNeeded    []string
-	AmountNeeded       []int
-	DropList           []string
-	DropRate           []int
+	DLC                bool     `json:"dlc"`
+	Name               string   `json:"name"`
+	BluePrint          bool     `json:"blueprint"`
+	BluePrintCostTable int      `json:"blueprint_cost_table"`
+	BluePrintCostBench int      `json:"blueprint_cost_bench"`
+	Workbench          bool     `json:"workbench"`
+	WorkbenchLevel     string   `json:"workbench_level"`
+	Stackable          int      `json:"stackable"`
+	Craftable          bool     `json:"craftable"`
+	AmountCraft        int      `json:"amount_craft"`
+	RessourceNeeded    []string `json:"ressource_needed"`
+	AmountNeeded       []int    `json:"amount_needed"`
+	DropList           []string `json:"droplist"`
+	DropRate           []int    `json:"droprate"`
 }
