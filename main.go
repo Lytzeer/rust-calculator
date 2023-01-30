3a65bb7fbe3ae211721e6408522f304839a161bc
plukas <lukas.portier@ynov.com>
2023-01-30 12:43:48 +0100

    Update Items struct

--- main.go
+++ main.go
@@ -7,20 +7,24 @@ import (
 )
 
 type Items struct {
-	DLC                bool     `json:"dlc"`
-	Name               string   `json:"name"`
-	BluePrint          bool     `json:"blueprint"`
-	BluePrintCostTable int      `json:"blueprint_cost_table"`
-	BluePrintCostBench int      `json:"blueprint_cost_bench"`
-	Workbench          bool     `json:"workbench"`
-	WorkbenchLevel     string   `json:"workbench_level"`
-	Stackable          int      `json:"stackable"`
-	Craftable          bool     `json:"craftable"`
-	AmountCraft        int      `json:"amount_craft"`
-	RessourceNeeded    []string `json:"ressource_needed"`
-	AmountNeeded       []int    `json:"amount_needed"`
-	DropList           []string `json:"droplist"`
-	DropRate           []int    `json:"droprate"`
+	Item []struct {
+		DLC                bool     `json:"dlc"`
+		Name               string   `json:"name"`
+		BluePrint          bool     `json:"blueprint"`
+		BluePrintCostTable int      `json:"blueprint_cost_table"`
+		BluePrintCostBench int      `json:"blueprint_cost_bench"`
+		Workbench          bool     `json:"workbench"`
+		WorkbenchLevel     string   `json:"workbench_level"`
+		Stackable          int      `json:"stackable"`
+		Craftable          bool     `json:"craftable"`
+		AmountCraft        int      `json:"amount_craft"`
+		RessourceNeeded    []string `json:"ressource_needed"`
+		AmountNeeded       []int    `json:"amount_needed"`
+		DropList           []string `json:"droplist"`
+		DropRate           []int    `json:"droprate"`
+		HasDurability      bool     `json:"has_durability"`
+		Durability         int      `json:"durability"`
+	} `json:"Item"`
 }
 
 type Structures struct {
